a864b7e803750876d0d3ae0f5a72be28be944785
Jon Shallcross <jshallcross1@icloud.com>
2021-03-03 15:18:10 -0500

    password hashing

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
     User.findAll({
-        attribute: {exclude: ['password']}
+        attributes: {exclude: ['password']}
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -55,12 +57,13 @@ router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body,{
+        individualHooks: true,
         where: {
             id:req.params.id
         }
     })
     .then(dbUserData =>{
-        if(!dbuserData[0]) {
+        if(!dbUserData[0]) {
             res.status(404).json({message:'No user found with this ID'});
             return;
         }

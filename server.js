21a02a1ddfcca8a9f47ea62a512d09c827df3920
Jon Shallcross <jshallcross1@icloud.com>
2021-03-10 12:47:33 -0500

    code clean up

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
